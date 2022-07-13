1f0136e54a1755783943e32e15fcb0f3387da9c6
itstor <ahmdthoriq5@gmail.com>
2022-07-13 21:04:46 +0700

    feat: init commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+import darkTheme from "../styles/themes";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextThemesProvider
+      defaultTheme="system"
+      attribute="class"
+      value={{
+        dark: darkTheme.className,
+      }}
+    >
+      <NextUIProvider>
+        <Component {...pageProps} />
+      </NextUIProvider>
+    </NextThemesProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/themes.jsx
@@ -0,0 +1,8 @@
+import { createTheme, NextUIProvider } from "@nextui-org/react";
+
+const darkTheme = createTheme({
+  type: "dark",
+  theme: {},
+});
+
+export default darkTheme;
