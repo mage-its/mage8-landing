590aa5b9c37202f75581cf6c2e8b43d65651f1d1
itstor <ahmdthoriq5@gmail.com>
2022-07-14 20:51:10 +0700

    feat: add custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGreen: "#A5D69E",
+      },
+    },
   },
   plugins: [],
 };
