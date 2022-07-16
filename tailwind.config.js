f6f4e467595dd6cf7a3e59e6c67879be3cda2efc
itstor <ahmdthoriq5@gmail.com>
2022-07-17 01:22:27 +0700

    feat: add gradient text animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,38 @@ module.exports = {
     extend: {
       backgroundImage: {
         "login-background": 'url("/static/images/login-background.png")',
+        "single-dark-background":
+          'url("/static/images/background-dark-single.jpg")',
+      },
+    },
+    animation: {
+      blob: "blob 7s infinite",
+      text: "text 5s ease infinite",
+    },
+    keyframes: {
+      blob: {
+        "0%": {
+          transform: "translate(0px, 0px) scale(1)",
+        },
+        "33%": {
+          transform: "translate(30px, -50px) scale(1.1)",
+        },
+        "66%": {
+          transform: "translate(-20px, 20px) scale(0.9)",
+        },
+        "100%": {
+          transform: "tranlate(0px, 0px) scale(1)",
+        },
+      },
+      text: {
+        "0%, 100%": {
+          "background-size": "200% 200%",
+          "background-position": "left center",
+        },
+        "50%": {
+          "background-size": "200% 200%",
+          "background-position": "right center",
+        },
       },
     },
   },
