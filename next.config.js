b29bd3dbd4574a056e64cf4acc178985e89dedbe
itstor <ahmdthoriq5@gmail.com>
2022-07-16 18:21:19 +0700

    fix: rewrite static aborted

--- next.config.js
+++ next.config.js
@@ -2,10 +2,14 @@
 const nextConfig = {
   async rewrites() {
     return {
-      beforeFiles: [
+      afterFiles: [
         {
           source: "/dashboard/:path*",
-          destination: "https://dashboard.mage-its.id/dashboard/:path*",
+          destination: "http://127.0.0.1:3001/:path*",
+        },
+        {
+          source: "/static/:path*",
+          destination: "http://127.0.0.1:3001/static/:path*",
         },
       ],
     };
