0f6f9a84d1e2ad2eb6b3df4c166b4595eed638d4
itstor <ahmdthoriq5@gmail.com>
2022-07-17 01:22:38 +0700

    feat: auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,74 @@
+import axios from "axios";
+
+const API_URL = process.env.BACKEND_API_URL + "v1/auth/";
+
+class AuthService {
+  async login(user) {
+    const response = await axios.post(`${API_URL}login`, {
+      email: user.email,
+      password: user.password,
+    });
+    return response.data;
+  }
+
+  googleLogin(idToken) {
+    return axios.post(`${API_URL}google-login`, {
+      idToken: idToken,
+    });
+  }
+
+  async logout() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    localStorage.removeItem("user");
+    if (!user) return;
+    const refreshToken = user.tokens.refresh.token;
+    await axios.post(`${API_URL}logout`, {
+      refreshToken,
+    });
+  }
+
+  async register(user) {
+    const response = await axios.post(`${API_URL}register`, user);
+    if (response.data.tokens) {
+      localStorage.setItem("user", JSON.stringify(response.data));
+    }
+    return response.data;
+  }
+
+  verifyEmailByToken(token) {
+    return axios.put(`${API_URL}verify-email/?token=${token}`);
+  }
+
+  requestChangePassword(email) {
+    return axios.post(`${API_URL}forgot-password`, { email });
+  }
+
+  resetPassword({ password, token }) {
+    return axios.post(`${API_URL}reset-password/?token=${token}`, { password });
+  }
+
+  requestVerificationEmail() {
+    return axios.post(`${API_URL}send-verification-email`);
+  }
+
+  async refreshToken() {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (!user) return;
+    const refreshToken = user.tokens.refresh.token;
+    const response = await axios.post(`${API_URL}refresh-tokens`, {
+      refreshToken,
+    });
+    if (response.data) {
+      const newUser = {
+        ...user,
+        tokens: {
+          ...response.data,
+        },
+      };
+      localStorage.setItem("user", JSON.stringify(newUser));
+    }
+    return response.data;
+  }
+}
+
+export default new AuthService();
