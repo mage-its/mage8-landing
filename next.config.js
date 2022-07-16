f3e3f9a21064ef51d110ef0e7553b64b4efe3f94
itstor <ahmdthoriq5@gmail.com>
2022-07-16 16:39:08 +0700

    feat: rewrite  dashboard

--- next.config.js
+++ next.config.js
@@ -1,5 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        {
+          source: "/:path*",
+          has: [
+            {
+              type: "host",
+              value: "dashboard.mage-its.id",
+            },
+          ],
+          destination: "/dashboard/:path*",
+        },
+      ],
+    };
+  },
   reactStrictMode: true,
   swcMinify: true,
 };
