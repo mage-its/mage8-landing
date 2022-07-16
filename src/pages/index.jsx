1f7b8e14b17eadeb4c5712ae748bd5f8225b669f
itstor <ahmdthoriq5@gmail.com>
2022-07-16 20:03:22 +0700

    refactor: change file type

---
+++ src/pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+import CompetitionCarrousel from "../components/CompetitionCarrousel";
+import { CompetitionData } from "../components/CompetitionData";
+import TitleSubbab from "../components/Title";
+import { Button, Collapse, Text, Grid } from "@nextui-org/react";
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div className="bg-black h-[100vh]">
+      <Grid.Container direction="column">
+        <CompetitionCarrousel compe={CompetitionData} />
+      </Grid.Container>
+    </div>
+  );
+}
