2ea60cae63895c7e46ee2f543971e06f2e9d6a79
Haq9815 <baihaq98ramadani@gmail.com>
2022-07-17 23:51:01 +0700

    seo seadanya

---
+++ src/components/Seo.jsx
@@ -0,0 +1,66 @@
+import { NextSeo } from "next-seo";
+export const SeoMain = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | Multimedia And Game Event 8"
+        description="MAGE (Multimedia and Game Event) adalah Wadah bagi generasi muda dalam berkreasi, berekspresi, dan berkompetisi untuk menuangkan inovasi sekaligus menyelesaikan permasalahan dengan memanfaatkan berbagai macam teknologi melalui serangkaian kegiatan seperti Development Competition (Apps, IoT, Games), Olimpiade, Workshop, dan Webinar."
+      />
+    </div>
+  );
+};
+
+export const SeoAbout = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | About"
+        description="MAGE (Multimedia and Game Event) adalah Wadah bagi generasi muda dalam berkreasi, berekspresi, dan berkompetisi untuk menuangkan inovasi sekaligus menyelesaikan permasalahan dengan memanfaatkan berbagai macam teknologi melalui serangkaian kegiatan seperti Development Competition (Apps, IoT, Games), Olimpiade, Workshop, dan Webinar."
+      />
+    </div>
+  );
+};
+
+export const SeoAppDev = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | Application Development"
+        description="Application Development adalah lomba pengembangan aplikasi untuk siswa SMA/SMK/Sederajat dan mahasiswa dalam mengembangkan ide kreatif untuk menyelesaikan masalah di Indonesia dalam bentuk aplikasi mobile, web dan desktop"
+      />
+    </div>
+  );
+};
+
+export const SeoGameDev = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | Game Development"
+        description="Game Development Competition merupakan sebuah ajang kompetisi pembuatan video game yang diselenggarakan oleh Teknik Komputer ITS. Game Development Competition ditujukan untuk siswa SMA/SMK/Sederajat dan mahasiswa dengan tujuan untuk mengembangkan ide dan inovasi anak-anak muda dalam bentuk karya game digital."
+      />
+    </div>
+  );
+};
+
+export const SeoIotDev = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | Internet of Things Development"
+        description="Internet Of Things Development Competition adalah lomba pengembangan teknologi berupa aplikasi (software) yang terintegrasi dengan alat (hardware) melalui jaringan internet dan sejenisnya yang dapat diikuti oleh masyarakat umum. Internet Of Things Development Competition bertujuan untuk mengembangkan ide kreatif yang dapat menyelesaikan masalah dengan hasil keluaran (output) berupa software dengan platform mobile disertai hardware-nya."
+      />
+    </div>
+  );
+};
+
+export const SeoOlimp = () => {
+  return (
+    <div>
+      <NextSeo
+        title="MAGE 8 | PROXY"
+        description="PROXY (Programming and Computer Competition x Computer Engineering ITS) merupakan sebuah ajang olimpiade yang diselenggarakan oleh Teknik Komputer ITS. PROXY bertujuan untuk menguji kemampuan siswa/i Indonesia dalam menyelesaikan soal-soal terkait logika dan pemrograman"
+      />
+    </div>
+  );
+};
