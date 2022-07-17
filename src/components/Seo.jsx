d6e457f8a942ff14f0902620a6f3075a319e207e
Haq9815 <baihaq98ramadani@gmail.com>
2022-07-18 00:15:17 +0700

    feat:additionalLinkTags

--- src/components/Seo.jsx
+++ src/components/Seo.jsx
@@ -5,6 +5,25 @@ export const SeoMain = () => {
       <NextSeo
         title="MAGE 8 | Multimedia And Game Event 8"
         description="MAGE (Multimedia and Game Event) adalah Wadah bagi generasi muda dalam berkreasi, berekspresi, dan berkompetisi untuk menuangkan inovasi sekaligus menyelesaikan permasalahan dengan memanfaatkan berbagai macam teknologi melalui serangkaian kegiatan seperti Development Competition (Apps, IoT, Games), Olimpiade, Workshop, dan Webinar."
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
@@ -16,6 +35,25 @@ export const SeoAbout = () => {
       <NextSeo
         title="MAGE 8 | About"
         description="MAGE (Multimedia and Game Event) adalah Wadah bagi generasi muda dalam berkreasi, berekspresi, dan berkompetisi untuk menuangkan inovasi sekaligus menyelesaikan permasalahan dengan memanfaatkan berbagai macam teknologi melalui serangkaian kegiatan seperti Development Competition (Apps, IoT, Games), Olimpiade, Workshop, dan Webinar."
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
@@ -27,6 +65,25 @@ export const SeoAppDev = () => {
       <NextSeo
         title="MAGE 8 | Application Development"
         description="Application Development adalah lomba pengembangan aplikasi untuk siswa SMA/SMK/Sederajat dan mahasiswa dalam mengembangkan ide kreatif untuk menyelesaikan masalah di Indonesia dalam bentuk aplikasi mobile, web dan desktop"
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
@@ -38,6 +95,25 @@ export const SeoGameDev = () => {
       <NextSeo
         title="MAGE 8 | Game Development"
         description="Game Development Competition merupakan sebuah ajang kompetisi pembuatan video game yang diselenggarakan oleh Teknik Komputer ITS. Game Development Competition ditujukan untuk siswa SMA/SMK/Sederajat dan mahasiswa dengan tujuan untuk mengembangkan ide dan inovasi anak-anak muda dalam bentuk karya game digital."
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
@@ -49,6 +125,25 @@ export const SeoIotDev = () => {
       <NextSeo
         title="MAGE 8 | Internet of Things Development"
         description="Internet Of Things Development Competition adalah lomba pengembangan teknologi berupa aplikasi (software) yang terintegrasi dengan alat (hardware) melalui jaringan internet dan sejenisnya yang dapat diikuti oleh masyarakat umum. Internet Of Things Development Competition bertujuan untuk mengembangkan ide kreatif yang dapat menyelesaikan masalah dengan hasil keluaran (output) berupa software dengan platform mobile disertai hardware-nya."
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
@@ -60,6 +155,25 @@ export const SeoOlimp = () => {
       <NextSeo
         title="MAGE 8 | PROXY"
         description="PROXY (Programming and Computer Competition x Computer Engineering ITS) merupakan sebuah ajang olimpiade yang diselenggarakan oleh Teknik Komputer ITS. PROXY bertujuan untuk menguji kemampuan siswa/i Indonesia dalam menyelesaikan soal-soal terkait logika dan pemrograman"
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "img/logo-mage 8.svg"
+          },
+          {
+            rel: "stylesheet",
+            href: "https://cdn.tailwindcss.com"
+          },
+          {
+            rel: "preload",
+            as: "style"
+          },
+          {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossOrigin: "anonymous"
+          }
+        ]}
       />
     </div>
   );
