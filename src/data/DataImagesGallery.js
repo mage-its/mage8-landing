3cda492d612865489eed2de05b31f2dc26555300
Haq9815 <baihaq98ramadani@gmail.com>
2022-07-21 18:24:22 +0700

    feat: add image gallery

---
+++ src/data/DataImagesGallery.js
@@ -0,0 +1,28 @@
+
+
+export const ImageGallery =[
+    {
+        img:'static/logo/images/gallery/1.png'
+    },
+    {
+        img:'static/logo/images/gallery/2.png'
+    },
+    {
+        img:'static/logo/images/gallery/3.png'
+    },
+    {
+        img:'static/logo/images/gallery/4.png'
+    },
+    {
+        img:'static/logo/images/gallery/5.png'
+    },
+    {
+        img:'static/logo/images/gallery/6.png'
+    },
+    {
+        img:'static/logo/images/gallery/7.png'
+    },
+    {
+        img:'static/logo/images/gallery/8.png'
+    }
+]
