7722fdeb6ddb034dc48bd4e88e4e50c1462bb856
Haq9815 <baihaq98ramadani@gmail.com>
2022-07-21 19:46:04 +0700

    fix name file

--- src/data/DataImagesGallery.js
+++ src/data/DataImagesGallery.js
@@ -1,28 +1,26 @@
-
-
-export const ImageGallery =[
-    {
-        img:'static/logo/images/gallery/1.png'
-    },
-    {
-        img:'static/logo/images/gallery/2.png'
-    },
-    {
-        img:'static/logo/images/gallery/3.png'
-    },
-    {
-        img:'static/logo/images/gallery/4.png'
-    },
-    {
-        img:'static/logo/images/gallery/5.png'
-    },
-    {
-        img:'static/logo/images/gallery/6.png'
-    },
-    {
-        img:'static/logo/images/gallery/7.png'
-    },
-    {
-        img:'static/logo/images/gallery/8.png'
-    }
-]
+export const ImageGallery = [
+  {
+    img: "static/logo/images/gallery/1.png"
+  },
+  {
+    img: "static/logo/images/gallery/2.jpg"
+  },
+  {
+    img: "static/logo/images/gallery/3.jpg"
+  },
+  {
+    img: "static/logo/images/gallery/4.JPG"
+  },
+  {
+    img: "static/logo/images/gallery/5.jpg"
+  },
+  {
+    img: "static/logo/images/gallery/6.jpg"
+  },
+  {
+    img: "static/logo/images/gallery/7.jpg"
+  },
+  {
+    img: "static/logo/images/gallery/8.png"
+  }
+];
