68e1aa2ade6dd99702ead0da8cd39c87a42fd98e
Haq9815 <baihaq98ramadani@gmail.com>
2022-07-21 19:59:14 +0700

    feat:add image gallery

--- src/data/DataImagesGallery.js
+++ src/data/DataImagesGallery.js
@@ -1,52 +1,26 @@
 export const ImageGallery = [
   {
-<<<<<<< HEAD
-    img: "static/logo/images/gallery/1.png"
+    img: "../assets/images/gallery/1.png"
   },
   {
-    img: "static/logo/images/gallery/2.jpg"
+    img: "../assets/images/gallery/2.jpg"
   },
   {
-    img: "static/logo/images/gallery/3.jpg"
+    img: "../assets/images/gallery/3.jpg"
   },
   {
-    img: "static/logo/images/gallery/4.JPG"
+    img: "../assets/images/gallery/4.JPG"
   },
   {
-    img: "static/logo/images/gallery/5.jpg"
+    img: "../assets/images/gallery/5.jpg"
   },
   {
-    img: "static/logo/images/gallery/6.jpg"
+    img: "../assets/images/gallery/6.jpg"
   },
   {
-    img: "static/logo/images/gallery/7.jpg"
+    img: "../assets/images/gallery/7.jpg"
   },
   {
-    img: "static/logo/images/gallery/8.png"
+    img: "../assets/images/gallery/8.png"
   }
-=======
-    img: "static/logo/images/gallery/1.png",
-  },
-  {
-    img: "static/logo/images/gallery/2.png",
-  },
-  {
-    img: "static/logo/images/gallery/3.png",
-  },
-  {
-    img: "static/logo/images/gallery/4.png",
-  },
-  {
-    img: "static/logo/images/gallery/5.png",
-  },
-  {
-    img: "static/logo/images/gallery/6.png",
-  },
-  {
-    img: "static/logo/images/gallery/7.png",
-  },
-  {
-    img: "static/logo/images/gallery/8.png",
-  },
->>>>>>> 57f8738bce67e8b2a07d59db56e7a503e669bf47
 ];
